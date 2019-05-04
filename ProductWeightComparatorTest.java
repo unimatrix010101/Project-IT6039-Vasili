e77df5e782495c2d93cd32a9d9e9e694c8b09eb2
unimatrix010101 <39468488+unimatrix010101@users.noreply.github.com>
2019-05-05 00:04:11 +1200

    Created ProductWeightComparatorTest Test file

---
+++ ProductWeightComparatorTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packer;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Vasili
+ */
+public class ProductWeightComparatorTest {
+    
+    public ProductWeightComparatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of compare method, of class ProductWeightComparator.
+     */
+    @Test
+    public void testCompare() {
+        System.out.println("compare");
+        Product a = null;
+        Product b = null;
+        ProductWeightComparator instance = new ProductWeightComparator();
+        int expResult = 0;
+        int result = instance.compare(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+    }
+    
+}

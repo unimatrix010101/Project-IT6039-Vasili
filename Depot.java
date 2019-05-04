8bb1a756bea4835f76ee73502728ee5b0b88b1cf
unimatrix010101 <39468488+unimatrix010101@users.noreply.github.com>
2019-05-04 23:05:59 +1200

    Fixed code
    Changed the following code
    
    from
    
    public String getName() {
           return address.toString();
       }
    
    to
    
    public String getName() {
            return name;
        }

---
+++ Depot.java
@@ -0,0 +1,38 @@
+package packer;
+
+/**
+ *
+ * @author Vasili
+ */
+public class Depot {
+    private String name;
+    private Address address;
+
+    public Depot(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+    /**
+     * Changed return to name; and removed .toString from 
+     * public String getName() {
+     *  return address.toString();
+    }
+     * As string needs to return name it was changed
+     * Furthermore the to.string is not needed as it is
+     * already part of a string
+     * @return 
+    */
+    public String getName() {
+        return name;
+    }
+    
+    public Coordinates getCoordinates() {
+        return this.address.getCoordinates();
+    }
+    
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+    
+}

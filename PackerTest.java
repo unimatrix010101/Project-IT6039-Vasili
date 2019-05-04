41f517330c5eeae27dddf64517dbfab54daed633
unimatrix010101 <39468488+unimatrix010101@users.noreply.github.com>
2019-05-04 23:58:41 +1200

    Created PackerTest Test file

---
+++ PackerTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packer;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Vasili
+ */
+public class PackerTest {
+    
+    public PackerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of packProducts method, of class Packer.
+     */
+    @Test
+    public void testPackProducts() {
+        System.out.println("packProducts");
+        Customer c = null;
+        Depot d = null;
+        Manifest m = null;
+        List<Box> expResult = null;
+        List<Box> result = Packer.packProducts(c, d, m);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
